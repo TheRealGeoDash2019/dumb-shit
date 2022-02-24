de92b3a3672a5879f75bcd0cc9990a9dd8af0ca9
TheRealGeoDash <TheRealGeoDash2019@gmail.com>
2022-02-24 18:46:30 -0500

    Create verify-headers.js

---
+++ verify-headers.js
@@ -0,0 +1,4 @@
+(() => {
+  console.log("%c[Discord Headers Bypass] %cEnabled Successfully!", "font-weight: 600; color: orange;", "font-weight: 400; color: white;");
+})();
+//# sourceURL=Discord Headers Bypass Extension
