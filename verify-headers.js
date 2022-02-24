8c0d7fc3c33a4cebd0aba9ddfb000ef76f239282
TheRealGeoDash <TheRealGeoDash2019@gmail.com>
2022-02-24 18:55:26 -0500

    Update verify-headers.js

--- verify-headers.js
+++ verify-headers.js
@@ -1,4 +1,7 @@
 (() => {
-  console.log("%c[Discord Headers Bypass] %cEnabled Successfully!", "font-weight: 600; color: orange;", "font-weight: 400; color: white;");
+  console.log("%c[Discord Headers Bypass] %cEnabled Successfully!", "font-weight: 600; color: green;", "font-weight: 400; color: white;");
+  setTimeout(() => {
+    document.querySelector("script[class='f626418c-e33c-40cf-ab16-757d260c14c5']").remove();
+  }, 3000);
 })();
 //# sourceURL=Discord Headers Bypass Extension
